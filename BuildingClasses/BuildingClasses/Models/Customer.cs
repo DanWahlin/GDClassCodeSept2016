ebe73fee24d35333830cb7ce96f70f72c46811cf
Dan Wahlin <dwahlin@xmlforasp.net>
2016-09-06 10:34:21 -0700

    Check-in BuildingClasses project

---
+++ BuildingClasses/BuildingClasses/Models/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingClasses.Models
+{
+    public class Customer
+    {
+        //Constructors
+        //ctor (tab + tab)
+        public Customer()
+        {
+            
+        }
+
+        public Customer(string name, string city, int age)
+        {
+            Name = name;
+            City = city;
+            Age = age;
+        }
+
+        //Field
+        string _name;
+
+        //Property (gateway or filters - like a door with a screen)
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length <= 10)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ApplicationException("Name is too long!");
+                }
+            }
+        }
+
+        //propfull (tab + tab)
+        private string _city;
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        //Method (function)
+        public string GetDetails()
+        {
+            return Name + " " + City;
+        }
+
+        //Method (subroutine)
+        //I'd use the property to set this
+        public void SetCity(string city)
+        {
+            City = city;
+        }
+
+        //prop (tab + tab)
+        //If you want logic, use propfull (tab + tab)
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ BuildingClasses/BuildingClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using BuildingClasses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cust = new Customer();
+            //SET block
+            cust.Name = "Dan Wahlin";
+            cust.Age = 24;
+            cust.City = "Ottawa";
+
+            //GET block
+            Console.WriteLine(cust.Name);
+
+            //Call Methods
+            var details = cust.GetDetails();
+            Console.WriteLine(details);
+
+            var cust2 = new Customer("Fred", "Phoenix", 55);
+            Console.WriteLine(cust2.GetDetails());
+
+            Console.ReadLine();
+        }
+    }
+}

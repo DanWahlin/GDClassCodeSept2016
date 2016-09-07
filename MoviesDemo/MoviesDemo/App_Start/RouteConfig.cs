306928b00839419b6db06cbd917a5be637a83350
Dan Wahlin <dwahlin@xmlforasp.net>
2016-09-07 10:00:37 -0700

    Added custom route

--- MoviesDemo/MoviesDemo/App_Start/RouteConfig.cs
+++ MoviesDemo/MoviesDemo/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MoviesDemo
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Flicks",
+                url: "flicks",
+                defaults: new { controller = "Movies", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
